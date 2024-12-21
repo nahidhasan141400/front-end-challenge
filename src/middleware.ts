74b438401773ff8751f857570b1b2381c976c0aa
Nahid Hasan <63138743+nahidhasan141400@users.noreply.github.com>
2024-12-21 12:28:33 +0600

    Fix: Implemented challenges

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,10 +3,8 @@ import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
   const user_agent = userAgent(request);
-
   const headers = new Headers(request.headers);
   headers.set("user_agent", user_agent.ua);
-
   return NextResponse.next({ headers });
 }
 
